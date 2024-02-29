efeefa0d96849cd75b9af897513353f634edf66f
Rajnish <RajnishAryan12@gmail.com>
2024-02-29 14:05:07 +0530

    minor issue fixed

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -2,27 +2,20 @@ import React from 'react';
 import "./dashboard.css"
 import Sidebar from "../Components/Sidebar/Sidebar"
 import Dashboard from '../Components/Dashboard/Dashboard'
-import BacklogCard from "../Components/card/BacklogCard"
-import DoneCard from "../Components/card/DoneCard"
-import ProgressCard from "../Components/card/ProgressCard"
 import { Navigate } from 'react-router-dom';
 
 const DashboardPage = () => {
- 
-
   const token = localStorage.getItem("token");
-
-
   if (!token) {
     return <Navigate to="/" />;
   }
   return (
     <>
-    <div className='dashboard-page' >
-      <Sidebar />
-      <Dashboard />
-      
-    </div>
+      <div className='dashboard-page' >
+        <Sidebar />
+        <Dashboard />
+
+      </div>
     </>
   )
 }
