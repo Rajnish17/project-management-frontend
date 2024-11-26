04952087e0b08fadd9a63917d25046d5fb81f296
Rajnish <rajnisharyan12@gmail.com>
2024-11-26 22:37:56 +0530

    email error fixed

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -73,6 +73,7 @@ const Login = () => {
             <input
               type="text"
               placeholder="Email"
+              value={email}
               className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               onChange={(e) => { setEmail(e.target.value) }}
             />
